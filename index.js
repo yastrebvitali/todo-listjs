d325ac0272293c5b8ee365848256bd0611791db1
yastrebvitali <yastrebvitali@gmail.com>
2021-12-24 15:27:22 +0300

    save 24

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ let btnCases = document.getElementById('btnCases');
 let casesWrapper = document.getElementById('casesWrapper');
 let tasks;
 let completeTaskAll;
-//let completeCasesAll = document.getElementById('completedCases');
-
+let completeCasesAll = document.getElementById('completedCases');
+let deleteTaskAll;
 //!localStorage.tasks ? tasks=[]:tasks=JSON.parse(localStorage.getItem('tasks'));
 
 
@@ -45,6 +45,15 @@ if (!localStorage.tasks) {
     tasks = JSON.parse(localStorage.getItem('tasks'));
 
 }
+
+if (!localStorage.deleteTaskAll) {
+    deleteTaskAll = [];
+    console.log(deleteTaskAll);
+} else {
+    deleteTaskAll = JSON.parse(localStorage.getItem('deleteTaskAll'));
+
+}
+
 //console.log(tasks);
 
 
@@ -56,36 +65,40 @@ function Task(description) {
 
 // Функия добавляет класс checkout
 const completeTask = index => {
-
     tasks[index].completed=!tasks[index].completed;
       if (tasks[index].completed) {
         LocalStore();
-                /* console.log(tasks);*/
-        // console.log( tasks[index].completed);
         completeTaskAll[index].classList.add('checkout');
     } else {
-        //tasks[index].completed=true;
         completeTaskAll[index].classList.remove('checkout');
-       LocalStore();
-       // fillHtmlList();
+        LocalStore();
 
-        /*console.log(tasks);*/
     }
 };
 
+const deleteTask = index => {
+    deleteTaskAll[index]= tasks[index];
+    console.log('deleteTaskAll[index]',deleteTaskAll[index]);
+    tasks.splice(index, 1);
+    LocalStore();
+    fillHtmlList();
+
+
+};
 
 //Создаем шаблон вставляем из массива таск
 let createTemplate = (tasks, index) => {
     return `<div class="task ${tasks.completed ? 'checkout':''} " > 
           <div>${tasks.description} </div>
           <input onclick="completeTask(${index})" type="checkbox" ${tasks.completed?'checked="checked"':''}>
-          <button class="btn" id="btnDelete">Удалить</button>
+          <button onclick="deleteTask(${index})" class="btn" id="btnDelete">Удалить</button>
         </div>`;
 };
 
 // Функция помещает Tasks(задание)в Localstore
 function LocalStore() {
-    localStorage.setItem('tasks', JSON.stringify(tasks))
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    localStorage.setItem('deleteTaskAll', JSON.stringify(deleteTaskAll));
 }
 
 
@@ -94,24 +107,34 @@ let fillHtmlList = () => {
     casesWrapper.innerHTML = " ";
     if (tasks.length > 0) {
         tasks.forEach((item, index) => {
-
             casesWrapper.innerHTML += createTemplate(item, index);
 
         });
         completeTaskAll = document.querySelectorAll('.task');
-        //console.log( completeTaskAll);
+
     }
 
+};
+// фУНКЦИЯ  Tasks(задание ) в html
+let fillDeleteTasks = () => {
+    completeCasesAll.innerHTML = " ";
+    if (deleteTaskAll.length > 0) {
+        deleteTaskAll.forEach((item, index) => {
+            completeCasesAll.innerHTML += createTemplate(item, index);
+
+        });
+      //  completeTaskAll = document.querySelectorAll('.task');
+
+    }
 
 };
 
+fillDeleteTasks();
 fillHtmlList();
 
 
 btnCases.addEventListener('click', () => {
-    //console.clear();
     tasks.push(new Task(enter_cases_input.value));
-    /*console.log('tasks',tasks);*/
     LocalStore();
     fillHtmlList();
 });
